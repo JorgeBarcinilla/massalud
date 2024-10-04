5476f5da17a2eb8813193295a1c543a285b08b88
Jorge Palacio Barcinilla <ctcfrontend@hotmail.com>
2024-10-04 02:43:55 -0500

    modificacion environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://b8x2b136-1337.usw3.devtunnels.ms/api' // Use production URL here such as where the Strapi CMS Hosted.
+  API_URL: 'https://srv609306.hstgr.cloud/api' // Use production URL here such as where the Strapi CMS Hosted.
 };
